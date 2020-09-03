6669de3a0abc3da26f3970ff0bd6495b81033aa0
Karen Helga Sigurgeirsdóttir <karenhelgasig@Karens-MacBook-Pro.local>
2020-09-03 15:37:08 +0000

    sequence done

--- sequence.py
+++ sequence.py
@@ -3,7 +3,6 @@
 # sequence:; 1, 2, 3, 6, 11, 20, 37
 
 
-
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 first=0
 second=1
@@ -11,10 +10,13 @@ third=2
 
 for i in range(0,n):
     if i == 0:
-         print(second)
+         print(second) # prints out one for the first number
     elif i == 1:
-        print(third)
+        print(third) # prints out two for the second number
     else :
+        # calculate next number, after 1 and 2
+        #  we sum the previous 3 numbers to get the next number
+        # for example for 3, first=0, second=1 and third=2
         next_number = first + second + third
         first = second
         second = third
